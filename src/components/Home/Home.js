eb44b9f499b193f79ef0f57bbc6cc60ba19a5a59
mleam <leam.marilyne@gmail.com>
2023-05-31 15:13:20 +0200

    modify home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,6 @@ import React from 'react'
 import HomeImg from '../../assets/home.jpg'
 import MapImg from '../../assets/map.png'
 import './Home.css'
-import { Link } from 'react-router-dom'
 
 function Home() {
     return (
